338fa566042434a18d9b7b654bc71a90a4b47cd4
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-06-12 14:12:29 +0100

    storybook addon a11y

--- boilerplate/.storybook/config.js
+++ boilerplate/.storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import { addDecorator } from '@storybook/react/dist/client/preview';
+
+addDecorator(withA11y);
 
 function loadStories() {
   require('../src/stories');
