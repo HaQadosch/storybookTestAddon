c5eccba19fdd01aacf0583734f97b2d259dcf1e2
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-06-12 14:15:57 +0100

    storybook addon background

--- boilerplate/.storybook/config.js
+++ boilerplate/.storybook/config.js
@@ -1,8 +1,15 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { addDecorator } from '@storybook/react/dist/client/preview';
 
 addDecorator(withA11y);
+addParameters({
+  backgrounds: [
+    { name: 'white', value: '#ffffff', default: true },
+    { name: 'twitter', value: '#00aced' },
+    { name: 'facebook', value: '#3b5998' },
+  ],
+});
 
 function loadStories() {
   require('../src/stories');
