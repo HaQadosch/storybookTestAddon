d9d2b26f70a66581b55f3b4695fd071cf1998a4a
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-06-14 14:28:59 +0100

    jsdom

--- boilerplate/src/__tests__/App.test.tsx
+++ boilerplate/src/__tests__/App.test.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { render } from '@testing-library/react';
+import 'jest-dom/extend-expect';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders Learn React message', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Learn React')).toBeInTheDocument();
+  });
 });
--- boilerplate/src/stories/Sample.stories.tsx
+++ boilerplate/src/stories/Sample.stories.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+// import { linkTo } from '@storybook/addon-links';
 import { text } from '@storybook/addon-knobs';
-import { Welcome } from '@storybook/react/demo';
 import Button from '@material-ui/core/Button';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add(
